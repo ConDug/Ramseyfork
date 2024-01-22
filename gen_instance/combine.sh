91a699d42c3feedeef25586becccbc206c77f81c
root <root@DESKTOP-5NDSUS7>
2024-01-22 14:32:13 -0500

    adding degree and edge constraints to file names

--- gen_instance/combine.sh
+++ gen_instance/combine.sh
@@ -2,15 +2,21 @@
 n=$1
 p=$2
 q=$3
-v=$4
-c=$5
+lower=${4:-0}
+upper=${5:-0}
+edge_b=${6:-0} #lower bound on triangles per blue edge
+Edge_b=${7:-0} #upper bound on triangles per blue edge
+edge_r=${8:-0} #lower bound on triangles per red edge
+Edge_r=${9:-0} 
+v=${10}
+c=${11}
 
 #Part of CNF file creation
 #v= total num var in cnf file
 #c= total num constraints in cnf file
 
-echo "p cnf ${v} ${c}" > ./temp_head_${n}_${p}_${q} #create cnf file and header
-cat ./temp_head_${n}_${p}_${q} ./constraints_temp_${n}_${p}_${q} > ./constraints_${n}_${p}_${q} #write constraints in temp_n_p_q to constraints
+echo "p cnf ${v} ${c}" > ./temp_head_${n}_${p}_${q}_${lower}_${upper}_${edge_b}_${Edge_b}_${edge_r}_${Edge_r} #create cnf file and header
+cat ./temp_head_${n}_${p}_${q}_${lower}_${upper}_${edge_b}_${Edge_b}_${edge_r}_${Edge_r} ./constraints_temp_${n}_${p}_${q}_${lower}_${upper}_${edge_b}_${Edge_b}_${edge_r}_${Edge_r} > ./constraints_${n}_${p}_${q}_${lower}_${upper}_${edge_b}_${Edge_b}_${edge_r}_${Edge_r} #write constraints in temp_n_p_q to constraints
 
-rm constraints_temp_${n}_${p}_${q}
-rm temp_head_${n}_${p}_${q}
+rm constraints_temp_${n}_${p}_${q}_${lower}_${upper}_${edge_b}_${Edge_b}_${edge_r}_${Edge_r}
+rm temp_head_${n}_${p}_${q}_${lower}_${upper}_${edge_b}_${Edge_b}_${edge_r}_${Edge_r}
